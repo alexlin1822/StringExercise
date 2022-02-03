5de2ce8663362561ffef6a4fa10c097144bb3f55
Alex Lin <alexlin1822@hotmail.com>
2022-02-03 11:50:32 -0500

    uploaded

--- training.stringassignment/src/main/java/training/stringassignment/MainClass.java
+++ training.stringassignment/src/main/java/training/stringassignment/MainClass.java
@@ -8,51 +8,50 @@ public class MainClass {
 		CallStringTokenizer();
 		CallStringBuffer();
 	}
-	
-	
+
 	public static void CallStringTokenizer() {
-		//StringTokenizer Function
-		//separated by spaces
-		String sR="Dajun Alex Lin";
-		System.out.print(sR+" -> ");
-		StringTokenizer st=new StringTokenizer(sR," ");
-		String sN="";
-		while (st.hasMoreTokens()){
-			sN+=st.nextToken()+":";
+		// StringTokenizer Function
+		// separated by spaces
+		String sR = "Dajun Alex Lin";
+		System.out.print(sR + " -> ");
+		StringTokenizer st = new StringTokenizer(sR, " ");
+		String sN = "";
+		while (st.hasMoreTokens()) {
+			sN += st.nextToken() + ":";
 		}
-		sN=sN.substring(0,sN.length()-1);
-		
+		sN = sN.substring(0, sN.length() - 1);
+
 		System.out.println(sN);
-		
+
 		System.out.println();
-				
-		//separated by _
-		sR="Dajun_Alex_Lin";
-		System.out.print(sR+" -> ");
-		StringTokenizer sk=new StringTokenizer(sR,"_");
-		while (sk.hasMoreTokens()){
-			System.out.print(sk.nextToken()+" ");
+
+		// separated by _
+		sR = "Dajun_Alex_Lin";
+		System.out.print(sR + " -> ");
+		StringTokenizer sk = new StringTokenizer(sR, "_");
+		while (sk.hasMoreTokens()) {
+			System.out.print(sk.nextToken() + " ");
 		}
 		System.out.println();
 		System.out.println();
-		
-		//separated by /
-		sR="10/12/2017";
-		System.out.println(sR+" -> ");
-		StringTokenizer sD=new StringTokenizer(sR,"/");
-		while (sD.hasMoreTokens()){
+
+		// separated by /
+		sR = "10/12/2017";
+		System.out.println(sR + " -> ");
+		StringTokenizer sD = new StringTokenizer(sR, "/");
+		while (sD.hasMoreTokens()) {
 			System.out.println(sD.nextToken());
 		}
 		System.out.println();
 	}
-	
+
 	public static void CallStringBuffer() {
-		//StringBuffer function
-		//Add something
+		// StringBuffer function
+		// Add something
 		StringBuffer sbf = new StringBuffer("Dajun Alex Lin");
 		System.out.println("Before : " + sbf);
-		  
-		sbf.insert(sbf.length()-1," 123");
+
+		sbf.insert(sbf.length() - 1, " 123");
 		sbf.append(" 3333");
 		System.out.println("After insert: " + sbf);
 	}
